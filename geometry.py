c37160df89016d42c669f9c648de6d3b15c6d121
Mohamed Wassim Tatari <wassimtatari@gmail.com>
2026-08-31 23:10:08 +0200

    Add files via upload

---
+++ geometry.py
@@ -0,0 +1,6 @@
+import math
+
+def circle_area(radius):
+    return math.pi*radius**2
+def rectangle_area(length,width):
+    return length*width
---
+++ import.py
@@ -0,0 +1,12 @@
+from geometry import circle_area,rectangle_area
+
+radius=5
+area_circle=circle_area(radius)
+
+print(f'Circle area is {area_circle:.2f}')
+
+length=5
+width=9
+area_rectang=rectangle_area(length,width)
+
+print(f"Rectangle area is {area_rectang}")
